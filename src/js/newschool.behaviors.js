ba2a08f9a27b963a81efb4e841bd279780ca6eee
Jen Breese-Kauth <jbreese@stanford.edu>
2022-01-05 09:14:11 -0800

    SLS22-9: added the breaks to the h2 based on a space

---
+++ src/js/newschool.behaviors.js
@@ -0,0 +1,39 @@
+/**
+ * Behavior Example that works with Webpack.
+ *
+ * @see: https://www.npmjs.com/package/drupal-behaviors-loader
+ *
+ * Webpack wraps everything in enclosures and hides the global variables from
+ * scripts so special handling is needed.
+ */
+
+export default {
+
+  // Attach Drupal Behavior.
+  attach (context, settings) {
+    // console.log("Attached.");
+    (function ($) {
+      var heading = $('.newschool--top-banner h2.newschool--heading-top-banner a');
+      
+      // add the breaks 
+      var html = $(heading).html().split(" ");
+       html = html[0] + '<br/>' + html[1] + '<br/>' + html[2];
+       $(heading).html(html);
+
+       // make new variable with the breaks.
+      var brokenheading = $('.newschool--top-banner h2.newschool--heading-top-banner');
+      
+      //move the heading with breaks
+      $(brokenheading).insertBefore('.newschool--top-banner article');
+      
+      //remove the link
+      $(".newschool--heading-top-banner a").contents().unwrap();
+
+    })(jQuery);
+  },
+
+  // Detach Example.
+  detach() {
+    // console.log("Detached.");
+  }
+};
