b289522066024522f747bb558ee3339cd271d42b
Jen Breese-Kauth <jbreese@stanford.edu>
2022-05-04 08:14:42 -0700

    removed the heading manipulation

--- src/js/newschool.behaviors.js
+++ src/js/newschool.behaviors.js
@@ -12,26 +12,26 @@ export default {
   // Attach Drupal Behavior.
   attach (context, settings) {
     // console.log("Attached.");
-    (function ($) {
+    //(function ($) {
 
-      if ($('.front').length) {
-        var heading = $('.newschool--top-banner h2.newschool--heading-top-banner a');
+      // if ($('.front').length) {
+        // var heading = $('.newschool--top-banner h2.newschool--heading-top-banner a');
       
-        // add the breaks
-        var html = $(heading).html().split(" ");
-        html = html[0] + '<br/>' + html[1] + '<br/>' + html[2];
-        $(heading).html(html);
+        // // add the breaks
+        // var html = $(heading).html().split(" ");
+        // html = html[0] + '<br/>' + html[1] + '<br/>' + html[2];
+        // $(heading).html(html);
 
         // make new variable with the breaks.
-        var brokenheading = $('.newschool--top-banner h2.newschool--heading-top-banner');
+        // var brokenheading = $('.newschool--top-banner h2.newschool--heading-top-banner');
 
         //move the heading with breaks
-        $(brokenheading).insertBefore('.newschool--top-banner article');
+        // $(brokenheading).insertBefore('.newschool--top-banner article');
 
         //remove the link
-        $(".newschool--heading-top-banner a").contents().unwrap();
-      }
-    })(jQuery);
+        // $(".newschool--heading-top-banner a").contents().unwrap();
+      // }
+    //})(jQuery);
   },
 
   // Detach Example.
