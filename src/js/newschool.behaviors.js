4eecfc4d69d78fec045bd9bd93cb2d6d5767800c
Jen Breese-Kauth <jbreese@stanford.edu>
2022-05-09 11:03:42 -0700

    EARTH-1642: fixes to the mobile display and removed the js for the header manipulation

--- src/js/newschool.behaviors.js
+++ src/js/newschool.behaviors.js
@@ -12,26 +12,6 @@ export default {
   // Attach Drupal Behavior.
   attach (context, settings) {
     // console.log("Attached.");
-    //(function ($) {
-
-      // if ($('.front').length) {
-        // var heading = $('.newschool--top-banner h2.newschool--heading-top-banner a');
-      
-        // // add the breaks
-        // var html = $(heading).html().split(" ");
-        // html = html[0] + '<br/>' + html[1] + '<br/>' + html[2];
-        // $(heading).html(html);
-
-        // make new variable with the breaks.
-        // var brokenheading = $('.newschool--top-banner h2.newschool--heading-top-banner');
-
-        //move the heading with breaks
-        // $(brokenheading).insertBefore('.newschool--top-banner article');
-
-        //remove the link
-        // $(".newschool--heading-top-banner a").contents().unwrap();
-      // }
-    //})(jQuery);
   },
 
   // Detach Example.
